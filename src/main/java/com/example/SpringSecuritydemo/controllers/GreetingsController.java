b75ef5ca40bb68cd2303137c7917af8c0f0dc434
Shahid <shahidnazirmhd@gmail.com>
2023-11-21 16:17:21 +0530

    21 Nov 2023

--- src/main/java/com/example/SpringSecuritydemo/controllers/GreetingsController.java
+++ src/main/java/com/example/SpringSecuritydemo/controllers/GreetingsController.java
@@ -1,16 +1,23 @@
 package com.example.SpringSecuritydemo.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/greetings")
+@RequestMapping("/api/v1")
 public class GreetingsController {
-    @GetMapping
+    @GetMapping(path = "greetings")
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Hello! from API ");
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping(path = "admin")
+    public ResponseEntity<String> getAdminData() {
+        return ResponseEntity.ok("This is Admin Data");
     }
 
     @GetMapping(path = "bye")
--- src/main/java/com/example/SpringSecuritydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/SpringSecuritydemo/security/ApplicationSecurityConfig.java
@@ -3,39 +3,48 @@ package com.example.SpringSecuritydemo.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
+@EnableMethodSecurity
 public class ApplicationSecurityConfig {
 
     @Bean
      public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) ->
             requests
                     .anyRequest()
-                    .authenticated();
-        });
-        http.httpBasic(Customizer.withDefaults());
+                    .authenticated())
+        .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
+    public UserDetailsService userDetailsService() {
         UserDetails user = User.builder()
                 .username("user")
                 .password(passwordEncoder().encode("password"))
                 .roles("USER")
                 .build();
-        return new InMemoryUserDetailsManager(user);
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("password123"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
     @Bean
